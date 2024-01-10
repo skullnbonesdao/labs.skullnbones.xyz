f4135e375c9bbc63a3183474ff32e8a3380237a2
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2024-01-10 20:07:22 +0000

    deploy: 14e8e8d6e5f59a1cf2e9ac755d09d1c67a9707b7

---
+++ assets/formatString.09555570.js
@@ -0,0 +1,1 @@
+function t(r){if(r)return r.filter(n=>n!==0).map(n=>String.fromCharCode(n)).join("")}function e(r){switch(r){case 1:return"MUD";case 2:return"ONI";case 3:return"UST"}}export{e as f,t as u};
